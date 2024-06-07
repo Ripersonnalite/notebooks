c2c08cf764c5b47c7a9519e70975356b50090350
ricardokazuo <105627320+ricardokazuo@users.noreply.github.com>
2024-06-07 16:21:26 +0900

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -40,6 +40,63 @@ def get_system_stats():
         "cpu_percent": cpu
     }
 
+
+
+
+# Define the Flask route for memory usage gauge
+@server.route('/cpu')
+def cpu_gauge():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    graph_json = create_gauge('CPU Usage', cpu_usage, 100)
+    
+    # HTML template string with embedded Plotly.js to render the gauge
+    html_string = f'''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
+    </head>
+    <body>
+        <button onclick="window.history.back();" class="back-button">Go Back</button>
+        <div id="gauge" style="width: 500px; height: 300px;"></div>
+        <script>
+            var gaugeData = {graph_json};
+            Plotly.newPlot('gauge', gaugeData.data, gaugeData.layout);
+            
+            // Function to fetch new memory usage data and update the gauge
+            function updateGauge() {{
+                fetch('/cpu-data')
+                .then(response => response.json())
+                .then(data => {{
+                    Plotly.update('gauge', {{
+                        'value': [data.percent]
+                    }}, {{
+                        'gauge': {{ 'axis': {{ 'range': [0, 100] }} }}
+                    }});
+                }});
+            }}
+            
+            // Set an interval to update the gauge every 5 seconds
+            setInterval(updateGauge, 5000);
+        </script>
+    </body>
+    </html>
+    '''
+    return render_template_string(html_string)
+
+# Define a new Flask route to provide memory usage data in JSON format
+@server.route('/cpu-data')
+def cpu_data():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    return {'percent': cpu_usage}
+
+
+
+
+
+
+
 # Define the Flask route for memory usage gauge
 @server.route('/memory')
 def memory_gauge():
@@ -130,6 +187,14 @@ def disk_gauge():
     '''
     return render_template_string(html_string)
 
+# Callback to update cpu gauge
+@app.callback(
+    Output('cpu-gauge', 'value'),
+    [Input('interval-component', 'n_intervals')]
+)
+def update_memory_gauge(n):
+    return get_system_stats()['cpu_percent']
+    
 # Callback to update memory gauge
 @app.callback(
     Output('memory-gauge', 'value'),
@@ -186,7 +251,7 @@ disk_card = dbc.Card(
             units="%",
             color={"gradient":True,"ranges":{"green":[0,60],"yellow":[60,80],"red":[80,100]}}
         ),
-            href="disk",  # Replace with your desired URL
+            href="/disk",  # Replace with your desired URL
         )
     ]),
     style={"width": "15rem", "height": "12rem", "margin": "5px"}
@@ -194,6 +259,7 @@ disk_card = dbc.Card(
 
 cpu_card = dbc.Card(
     dbc.CardBody([
+        html.A(
         daq.Gauge(
             id='cpu-gauge',
             label="AMD Ryzen 7 5700U",
@@ -204,6 +270,8 @@ cpu_card = dbc.Card(
             showCurrentValue=True,
             units="%",
             color={"gradient":True,"ranges":{"green":[0,60],"yellow":[60,80],"red":[80,100]}}
+        ),
+            href="/cpu",  # Replace with your desired URL
         )
     ]),
     style={"width": "15rem", "height": "12rem", "margin": "5px"}
@@ -214,7 +282,7 @@ outer_card = dbc.Card(
         dbc.Row([
             dbc.Col(ThemeSwitchAIO(aio_id="theme", themes=[url_theme1, url_theme2]), sm=4)
         ]),
-        html.H3("System Usage Dashboard", className="card-title", style={'textAlign': 'center'}),
+        html.H3("System Resources Dashboard", className="card-title", style={'textAlign': 'center'}),
         dbc.Row([
             dbc.Col(memory_card, width="auto"),
             dbc.Col(disk_card, width="auto"),
